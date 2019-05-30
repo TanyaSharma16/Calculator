c1cac9db69ddd4b230da49857b3a6063f788b4b3
tanya <tsharma@tec-centers.com>
2019-05-30 16:34:15 -0400

    Requirement 1- Completed
    Allow the user to define the color of the results.

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -17,38 +17,46 @@ namespace ConsoleCalculator
             Console.Write("Now, Please type the second operand: ");
             var n2 = Console.ReadLine();
 
+            Console.WriteLine("Please choose the color of results from the following options:\n Red\n Blue\n Green\n Yellow");
+            var colorSelected = Console.ReadLine();
             var calculator = new Calculator();
+            switch (colorSelected)
+            {
+                case "Red":
+                    calculator.color = ConsoleColor.Red;
+                        break;
+                case "Blue":
+                    calculator.color = ConsoleColor.Blue;
+                    break;
+                case "Green":
+                    calculator.color = ConsoleColor.Green;
+                    break;
+                case "Yellow":
+                    calculator.color = ConsoleColor.Yellow;
+                    break;
+                default:
+                    calculator.color = ConsoleColor.White;
+                    break;
 
+
+            }
             /*
              if-else statements should be changed to switch statement
              */
-
             if (result == "sum")
-            {
-                calculator.color = ConsoleColor.Red;
                 calculator.Sum(Convert.ToDouble(n1), Convert.ToDouble(n2));
-            }
             else
             {
                 if (result == "subtract")
-                {
-                    calculator.color = ConsoleColor.Red;
                     calculator.Subtract(Convert.ToDouble(n1), Convert.ToDouble(n2));
-                }
                 else
                 {
                     if (result == "multiply")
-                    {
-                        calculator.color = ConsoleColor.Red;
                         calculator.Multiply(Convert.ToDouble(n1), Convert.ToDouble(n2));
-                    }
                     else
                     {
                         if (result == "divide")
-                        {
-                            calculator.color = ConsoleColor.Red;
                             calculator.Divide(Convert.ToDouble(n1), Convert.ToDouble(n2));
-                        }
                     }
                 }
             }
